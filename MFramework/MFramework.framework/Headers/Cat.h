6efc790a3403046af412896ea27df1b23133dd57
zhangxuewei <303453384@qq.com>
2024-08-01 15:31:17 +0800

    add mframework

---
+++ MFramework/MFramework.framework/Headers/Cat.h
@@ -0,0 +1,16 @@
+//
+//  Cat.h
+//  MFramework
+//
+//  Created by zhangxuewei on 2024/7/23.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Cat : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
